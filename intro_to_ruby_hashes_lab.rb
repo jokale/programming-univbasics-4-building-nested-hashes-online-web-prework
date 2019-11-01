ce6cf8f24f450fa8fb8772400928d5df13b77aa6
Joanna E K <kalemajoanna@gmail.com>
2019-11-01 21:59:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,6 +17,7 @@ def monopoly_with_third_tier
 	    one_piece_owned: 25,
 	    two_pieces_owned: 50,
 	    three_pieces_owned: 100,
+<<<<<<< HEAD
 	    four_pieces_owned: 200
 	    },
 	    names:{
@@ -24,6 +25,15 @@ def monopoly_with_third_tier
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline_railroad: {}
+=======
+	    {} 
+	    },
+	    names:{
+	      koko: {},
+	      loko: {},
+	      joko: {},
+	      hoko: {}
+>>>>>>> bcbfa6efff23fb8c98d1c5b61eb915d23d94eabb
 	    }
 	 	}
  	}
